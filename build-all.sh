291b8292e7e2d7466f5a79619885a98185d418dc
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-12 11:16:28 +0300

    update build-all.sh
    I use different username on my PC, so need to update
    conditional for executing image publish steps.

--- build-all.sh
+++ build-all.sh
@@ -3,7 +3,7 @@
 docker build -t 'xeffyr/termux:latest' -f Dockerfile.32bit .
 docker build -t 'xeffyr/termux:x86_64' -f Dockerfile.64bit .
 
-if [ "$(whoami)" = "xeffyr" ]; then
+if [ "${1-}" = "publish" ]; then
 	docker push 'xeffyr/termux:latest'
 	docker push 'xeffyr/termux:x86_64'
 fi
