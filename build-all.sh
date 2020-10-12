26c378b9efef2b8d0c72daa27e6f75322b48c372
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-12 11:19:52 +0300

    build-all.sh: use sudo for Docker commands

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -t 'xeffyr/termux:latest' -f Dockerfile.32bit .
-docker build -t 'xeffyr/termux:x86_64' -f Dockerfile.64bit .
+sudo docker build -t 'xeffyr/termux:latest' -f Dockerfile.32bit .
+sudo docker build -t 'xeffyr/termux:x86_64' -f Dockerfile.64bit .
 
 if [ "${1-}" = "publish" ]; then
-	docker push 'xeffyr/termux:latest'
-	docker push 'xeffyr/termux:x86_64'
+	sudo docker push 'xeffyr/termux:latest'
+	sudo docker push 'xeffyr/termux:x86_64'
 fi
