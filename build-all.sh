887050accc2fb66ba3681437e1355074f21f9925
rojenzaman <rojen@riseup.net>
2020-12-23 02:12:14 +0300

    add 'run podman with --podman or -p argument'

--- build-all.sh
+++ build-all.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+OCI="docker"
+case $1 in
+    -p|--podman) OCI="podman" ;;
+esac
+
 if [ -n "${TERMUX_DOCKER_USE_SUDO-}" ]; then
 	SUDO="sudo"
 else
 	SUDO=""
 fi
 
-$SUDO docker build -t 'xeffyr/termux:latest' -f Dockerfile.32bit .
-$SUDO docker build -t 'xeffyr/termux:x86_64' -f Dockerfile.64bit .
+$SUDO $OCI build -t 'docker.io/xeffyr/termux:latest' -f Dockerfile.32bit .
+$SUDO $OCI build -t 'docker.io/xeffyr/termux:x86_64' -f Dockerfile.64bit .
 
 if [ "${1-}" = "publish" ]; then
-	$SUDO docker push 'xeffyr/termux:latest'
-	$SUDO docker push 'xeffyr/termux:x86_64'
+	$SUDO $OCI push 'docker.io/xeffyr/termux:latest'
+	$SUDO $OCI push 'docker.io/xeffyr/termux:x86_64'
 fi
--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+case $1 in
+    -p|--podman) shopt -s expand_aliases ; alias docker="podman" ;;
+esac
+
 if [ "$(basename "$0")" = "run-x86_64.sh" ]; then
 	CONTAINER_NAME="termux-x86_64"
-	DOCKER_IMAGE_NAME="xeffyr/termux:x86_64"
+	DOCKER_IMAGE_NAME="docker.io/xeffyr/termux:x86_64"
 else
 	CONTAINER_NAME="termux-i686"
-	DOCKER_IMAGE_NAME="xeffyr/termux:latest"
+	DOCKER_IMAGE_NAME="docker.io/xeffyr/termux:latest"
 fi
 
 docker start "$CONTAINER_NAME" > /dev/null 2> /dev/null || {
