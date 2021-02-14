b12229c476c59a177cd8450664ed71a4a80fb43c
umarcor <unai.martinezcorral@ehu.eus>
2021-02-14 19:03:31 +0100

    use a single Dockerfile with build-args

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,7 @@ set -e
 
 OCI="docker"
 case $1 in
-    -p|--podman) OCI="podman" ;;
+	-p|--podman) OCI="podman" ;;
 esac
 
 if [ -n "${TERMUX_DOCKER_USE_SUDO-}" ]; then
@@ -13,8 +13,15 @@ else
 	SUDO=""
 fi
 
-$SUDO $OCI build -t 'docker.io/xeffyr/termux:latest' -f Dockerfile.32bit .
-$SUDO $OCI build -t 'docker.io/xeffyr/termux:x86_64' -f Dockerfile.64bit .
+for arch in "i686" "x86_64"; do
+	$SUDO $OCI build \
+		-t 'docker.io/xeffyr/termux:'"$arch" \
+		-f Dockerfile \
+		--build-arg BOOTSTRAP_ARCH="$arch" \
+		.
+done
+
+docker tag docker.io/xeffyr/termux:i686 docker.io/xeffyr/termux:latest
 
 if [ "${1-}" = "publish" ]; then
 	$SUDO $OCI push 'docker.io/xeffyr/termux:latest'
