bfa47d85fce4e25892c6e29def9c66efe7705270
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-15 23:02:48 +0200

    initial support for arm/arm64 containers
    Issue https://github.com/termux/termux-docker/issues/9.
    
    System components are imported from Android 9 GSI (aarch64).

--- build-all.sh
+++ build-all.sh
@@ -13,17 +13,32 @@ else
 	SUDO=""
 fi
 
-for arch in "i686" "x86_64"; do
+case "$(uname -m)" in
+	aarch64) SYSTEM_TYPE="arm"; ARCHITECTURES=("aarch64" "arm");;
+	armv7l|armv8l) SYSTEM_TYPE="arm"; ARCHITECTURES=("arm");;
+	i686) SYSTEM_TYPE="x86"; ARCHITECTURES=("i686");;
+	x86_64) SYSTEM_TYPE="x86"; ARCHITECTURES=("i686" "x86_64");;
+	*)
+		echo "'uname -m' returned unknown architecture"
+		exit 1
+		;;
+esac
+
+for arch in "${ARCHITECTURES[@]}"; do
 	$SUDO $OCI build \
 		-t 'docker.io/xeffyr/termux:'"$arch" \
 		-f Dockerfile \
 		--build-arg BOOTSTRAP_ARCH="$arch" \
+		--build-arg SYSTEM_TYPE="$SYSTEM_TYPE"
 		.
+	if [ "${1-}" = "publish" ]; then
+		$SUDO $OCI push 'docker.io/xeffyr/termux:'"$arch"
+	fi
 done
 
-docker tag docker.io/xeffyr/termux:i686 docker.io/xeffyr/termux:latest
-
-if [ "${1-}" = "publish" ]; then
-	$SUDO $OCI push 'docker.io/xeffyr/termux:latest'
-	$SUDO $OCI push 'docker.io/xeffyr/termux:x86_64'
+if [ "$SYSTEM_TYPE" = "x86" ]; then
+	docker tag docker.io/xeffyr/termux:i686 docker.io/xeffyr/termux:latest
+	if [ "${1-}" = "publish" ]; then
+		$SUDO $OCI push 'docker.io/xeffyr/termux:latest'
+	fi
 fi
